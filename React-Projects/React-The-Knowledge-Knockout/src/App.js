d749f04f87d922170d392f7107902157d0d1d66c
AAYUSH DUBEY <aayush200dubey@gmail.com>
2024-10-13 08:34:29 +0530

    Add files via upload

---
+++ React-Projects/React-The-Knowledge-Knockout/src/App.js
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+
+export default function App() {
+	const questions = [
+		{
+			questionText: 'Who is the current prime minister of India?',
+			answerOptions: [
+				{ answerText: 'Rahul Gandhi', isCorrect: false },
+				{ answerText: 'Nitin Gadkari', isCorrect: false },
+				{ answerText: 'Narendra Modi', isCorrect: true },
+				{ answerText: 'Amit Shah', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Who is known as the "Father of the Indian Constitution"?',
+			answerOptions: [
+				{ answerText: 'Mahatma Gandhi', isCorrect: false },
+				{ answerText: 'B.R. Ambedkar', isCorrect: true },
+				{ answerText: 'Sardar Vallabhbhai Patel', isCorrect: false },
+				{ answerText: 'Jawahar Lal Nehru', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'What is the maximum strength of the Lok Sabha?',
+			answerOptions: [
+				{ answerText: '545', isCorrect: true },
+				{ answerText: '500', isCorrect: false },
+				{ answerText: '552', isCorrect: false },
+				{ answerText: '600', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Which political party is represented by the symbol of a lotus?',
+			answerOptions: [
+				{ answerText: 'Indian National Congress', isCorrect: false },
+				{ answerText: 'Aam Aadmi Party', isCorrect: false },
+				{ answerText: 'Samajwadi Party', isCorrect: false },
+				{ answerText: 'Bharatiya Janata Party', isCorrect: true },
+			],
+		},
+	];
+
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [showScore, setShowScore] = useState(false);
+	const [score, setScore] = useState(0);
+
+	const handleAnswerOptionClick = (isCorrect) => {
+		if (isCorrect) {
+			setScore(score + 1);
+		}
+
+		const nextQuestion = currentQuestion + 1;
+		if (nextQuestion < questions.length) {
+			setCurrentQuestion(nextQuestion);
+		} else {
+			setShowScore(true);
+		}
+	};
+	return (
+		<div className='app'>
+			{showScore ? (
+				<div className='score-section'>
+					You scored {score} out of {questions.length}
+				</div>
+			) : (
+				<>
+					<div className='question-section'>
+						<div className='question-count'>
+							<span>Question {currentQuestion + 1}</span>/{questions.length}
+						</div>
+						<div className='question-text'>{questions[currentQuestion].questionText}</div>
+					</div>
+					<div className='answer-section'>
+						{questions[currentQuestion].answerOptions.map((answerOption) => (
+							<button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+						))}
+					</div>
+				</>
+			)}
+		</div>
+	);
+}
